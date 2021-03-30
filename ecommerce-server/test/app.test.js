0aeaf9693f5266a07becc403a21bf015d459591c
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2021-03-30 18:14:38 +0300

    updated test to reflect 403

--- ecommerce-server/test/app.test.js
+++ ecommerce-server/test/app.test.js
@@ -3,12 +3,12 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('app', () => {
-  it('responds with a not found message', (done) => {
+  it('responds with a not authorized message', (done) => {
     request(app)
       .get('/what-is-this-even')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(404, done);
+      .expect(403, done);
   });
 });
 
