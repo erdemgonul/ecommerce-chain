a49dbf246899518afa8b437698ece3abe2298782
nadiryuceer <nadiryuceer@users.noreply.github.com>
2021-03-30 22:28:55 +0300

    Sign-in and Sign-up functionality tests are written and run.

--- ecommerce-server/test/api.test.js
+++ ecommerce-server/test/api.test.js
@@ -2,14 +2,56 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
+let token = null;
 describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: 'API - 👋🌎🌍🌏'
-      }, done);
-  });
+    it('responds with a json message', (done) => {
+        request(app)
+            .get('/api/v1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, {
+                message: 'API - 👋🌎🌍🌏'
+            }, done);
+    });
+});
+
+describe('signup', () => {
+    it('signs up the user', (done) => {
+        request(app)
+            .post('/api/v1/auth/signup')
+            .send({
+                "userName": "nadir",
+                "firstName": "nadir",
+                "lastName": "yuceer",
+                "email": "nadir@gmail.com",
+                "password": "123456"
+            })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, {"success": true})
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                return done();
+            });
+    });
+});
+
+describe('signin', () => {
+    it('responds with accesstoken', (done) => {
+        request(app)
+            .post('/api/v1/auth/signin')
+            .send({"userName":"nadir","password":"123456"})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                token=res.accessToken;
+                return done();
+            });
+    });
 });
