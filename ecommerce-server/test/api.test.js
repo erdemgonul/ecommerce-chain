2f3229888fe05c7935d1b484c32e33f73528d2d0
nadiryuceer <nadiryuceer@users.noreply.github.com>
2021-04-01 20:43:18 +0300

    ApiTestUpdate: Success and confirmation tests of changing user information are completed.

--- ecommerce-server/test/api.test.js
+++ ecommerce-server/test/api.test.js
@@ -7,35 +7,21 @@ var successcheck = function (res) {
     if (res.body.success === false) return new Error("success false");
 }
 
-var datacheck = function (res){
+var datacheck = function (res) {
     var temp = res.body.data;
-    if(temp.username!=="nadir" || temp.firstName!=="nadir" || temp.lastName!=="yuceer" || temp.email !== "nadir@gmail.com") return new Error("data does not match");
+    if (temp.username !== "nadir" || temp.firstName !== "nadir" || temp.lastName !== "yuceer" || temp.email !== "nadir@gmail.com") return new Error("data does not match");
 }
-
-
-function signin() {
-    it('responds with accesstoken', (done) => {
-        request(app)
-            .post('/api/v1/auth/signin')
-            .send({"userName": "nadir", "password": "123456"})
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .expect(successcheck)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                token = res.body.accessToken;
-                return done();
-            });
-    });
+var datacheckafter = function (res) {
+    var temp = res.body.data;
+    if (temp.username !== "ahmet" || temp.firstName !== "nadir" || temp.lastName !== "yuceer" || temp.email !== "nadir@gmail.com") return new Error("data does not match");
 }
+const rootdir = "/api/v1";
+
 
 describe('GET /api/v1', () => {
     it('responds with a json message', (done) => {
         request(app)
-            .get('/api/v1')
+            .get(rootdir)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, {
@@ -44,10 +30,10 @@ describe('GET /api/v1', () => {
     });
 });
 
-describe('signup', () => {
-    it('signs up the user', (done) => {
+describe('Sign-in, Sign-up and user_info management', () => {
+    it('signup', (done) => {
         request(app)
-            .post('/api/v1/auth/signup')
+            .post(rootdir + '/auth/signup')
             .send({
                 "userName": "nadir",
                 "firstName": "nadir",
@@ -65,29 +51,55 @@ describe('signup', () => {
                 return done();
             });
     });
-});
-
-describe('signin', () => {
-    signin();
-});
-
-describe('getuserdetailswithtoken', () => {
-    signin();
-    it('responds with data', (done) => {
+    it('sign-in responds with accesstoken', (done) => {
         request(app)
-            .post('/api/v1/user/get/details')
+            .post(rootdir + '/auth/signin')
+            .send({"userName": "nadir", "password": "123456"})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(successcheck)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                token = res.body.accessToken;
+                return done();
+            });
+    });
+    it('getuserdetails', (done) => {
+        request(app)
+            .post(rootdir + '/user/get/details')
             .send({"userName": "nadir"})
             .set('Authorization', 'Bearer ' + token)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
-            .expect(datacheck)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                token = res.accessToken;
-                return done();
-            });
+            .expect(datacheck,done())
+    });
+    it('setuserdetails', (done) => {
+
+        let data = {
+            "firstName": "ahmet",
+        }
+        request(app)
+            .post(rootdir + '/user/get/details')
+            .send(data)
+            .set('Authorization', 'Bearer ' + token)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(successcheck, done())
+
+    });
+    it('getuserdetailsafterchange', (done) => {
+        request(app)
+            .post(rootdir + '/user/get/details')
+            .send({"userName": "nadir"})
+            .set('Authorization', 'Bearer ' + token)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(datacheckafter,done())
     });
 });
