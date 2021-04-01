f391902a6106d05b9995e28d7c7af423c7194454
nadiryuceer <nadiryuceer@users.noreply.github.com>
2021-04-01 20:01:02 +0300

    getting UserDetails test with username added.

--- ecommerce-server/test/api.test.js
+++ ecommerce-server/test/api.test.js
@@ -3,6 +3,35 @@ const request = require('supertest');
 const app = require('../src/app');
 
 let token = null;
+var successcheck = function (res) {
+    if (res.body.success === false) return new Error("success false");
+}
+
+var datacheck = function (res){
+    var temp = res.body.data;
+    if(temp.username!=="nadir" || temp.firstName!=="nadir" || temp.lastName!=="yuceer" || temp.email !== "nadir@gmail.com") return new Error("data does not match");
+}
+
+
+function signin() {
+    it('responds with accesstoken', (done) => {
+        request(app)
+            .post('/api/v1/auth/signin')
+            .send({"userName": "nadir", "password": "123456"})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(successcheck)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                token = res.body.accessToken;
+                return done();
+            });
+    });
+}
+
 describe('GET /api/v1', () => {
     it('responds with a json message', (done) => {
         request(app)
@@ -39,18 +68,25 @@ describe('signup', () => {
 });
 
 describe('signin', () => {
-    it('responds with accesstoken', (done) => {
+    signin();
+});
+
+describe('getuserdetailswithtoken', () => {
+    signin();
+    it('responds with data', (done) => {
         request(app)
-            .post('/api/v1/auth/signin')
-            .send({"userName":"nadir","password":"123456"})
+            .post('/api/v1/user/get/details')
+            .send({"userName": "nadir"})
+            .set('Authorization', 'Bearer ' + token)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+            .expect(datacheck)
             .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
-                token=res.accessToken;
+                token = res.accessToken;
                 return done();
             });
     });
