08f92793d1c1890c1a885a2858b8900266260c79
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2021-05-01 19:04:50 +0300

    added fuzzyness to es

--- ecommerce-server/src/config/authorization.config.js
+++ ecommerce-server/src/config/authorization.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   noAuthEndpoints: ['/api/v1', '/api/v1/auth/signup', '/api/v1/auth/signin', '/api/v1/product/suggestedproducts',
     '/api/v1/category/get/subcategories', '/api/v1/category/get/details', '/api/v1/product/get/category',
-    '/api/v1/product/search', '/api/v1/product/get', '/api/v1/product/get/category/filter'],
+    '/api/v1/product/search', '/api/v1/product/get', '/api/v1/product/get/category/filter', '/api/v1/category/get/all'],
   productManagerEndpoints: ['/api/v1/product/create', '/api/v1/category/create', '/api/v1/product/delete'],
 };
