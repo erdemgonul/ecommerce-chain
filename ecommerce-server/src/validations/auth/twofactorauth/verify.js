6a6cb2914322396ff00fbb4683f1aaafba6e95ea
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2021-06-02 18:50:27 +0300

    verify validation fix

--- ecommerce-server/src/validations/auth/twofactorauth/verify.js
+++ ecommerce-server/src/validations/auth/twofactorauth/verify.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const schema = Joi.object({
   userName: Joi.string().required(),
-  twoFactorCode: Joi.number().min(6).max(6).required(),
+  twoFactorCode: Joi.number().min(100000).max(999999).required(),
 });
 
 module.exports = schema;
