eb18ca0cc59f0955709b642bbd3486fc2eac8544
OguzErtanAyanlar <oguzertanayanlar@gmail.com>
2021-06-03 18:33:42 +0300

    sales manager cancel order validation

--- ecommerce-server/src/config/validation.config.js
+++ ecommerce-server/src/config/validation.config.js
@@ -4,5 +4,5 @@ module.exports = {
     '/api/v1/product/search', '/api/v1/product/get', '/api/v1/product/delete', '/api/v1/product/get/category/filter', '/api/v1/category/get/filters',
     '/api/v1/order/delete', '/api/v1/order/create', '/api/v1/order/get', '/api/v1/invoice/get', '/api/v1/order/finish',
     '/api/v1/comment/delete', '/api/v1/comment/create', '/api/v1/comment/get/approved/all', '/api/v1/auth/twofactorauth/verify', '/api/v1/comment/get/all',
-  '/api/v1/comment/get/nonapproved/all','/api/v1/comment/approve','/api/v1/payment/fund','/api/v1/order/edit'],
+  '/api/v1/comment/get/nonapproved/all','/api/v1/comment/approve','/api/v1/payment/fund','/api/v1/order/edit', '/api/v1/order/cancel'],
 };
