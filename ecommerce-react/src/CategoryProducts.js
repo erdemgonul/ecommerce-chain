2a88cbeac7189949403532ccc522b0207ef00dad
erdemgonul <ahmeterdemgonul@gmail.com>
2021-06-03 12:23:35 +0300

    filter fix , sort by price

--- ecommerce-react/src/CategoryProducts.js
+++ ecommerce-react/src/CategoryProducts.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import HomeProduct from "./HomeProduct";
-import axios from 'axios';
-import {
-  useHistory,
-} from "react-router-dom";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function CategoryProducts() {
-  const history = useHistory()
+  const history = useHistory();
   const [products, setProducts] = useState([]);
   const [filters, setFilters] = useState(null);
   const [valueFilter, setValueFilter] = useState({});
@@ -14,49 +12,92 @@ function CategoryProducts() {
   const [category, setCategory] = useState("");
 
   useEffect(() => {
-    setCategory(window.location.href.slice(window.location.href.lastIndexOf('categories/') + 11, window.location.href.length));
+    setCategory(
+      window.location.href.slice(
+        window.location.href.lastIndexOf("categories/") + 11,
+        window.location.href.length
+      )
+    );
     getFirst();
   }, []);
   useEffect(() => {
-    setCategory(window.location.href.slice(window.location.href.lastIndexOf('categories/') + 11, window.location.href.length));
+    setCategory(
+      window.location.href.slice(
+        window.location.href.lastIndexOf("categories/") + 11,
+        window.location.href.length
+      )
+    );
     return history.listen(() => {
       setValueFilter({});
       getFirst();
-    })
+    });
   }, [history]);
   function changeFilter(key, value) {
     if (value.target.name == "priceMin" || value.target.name == "priceMax") {
-      value.target.value = parseFloat(value.target.value);
+      if (value.target.value == "") {
+        value.target.value = 0;
+      } else value.target.value = parseFloat(value.target.value);
+    } else {
+      valueFilter[value.target.name] = value.target.value;
+      getProductsByFilter();
     }
-    valueFilter[value.target.name] = parseFloat(value.target.value);
-    getProductsByFilter();
   }
   async function getFirst() {
-    axios.post(`${process.env.REACT_APP_ENDPOINT_URL}/api/v1/product/get/category`, {
-      "path": window.location.href.slice(window.location.href.lastIndexOf('categories/') + 11, window.location.href.length),
-      "strictMode": false
-    })
-      .then(res => {
+    axios
+      .post(
+        `${process.env.REACT_APP_ENDPOINT_URL}/api/v1/product/get/category`,
+        {
+          path: window.location.href.slice(
+            window.location.href.lastIndexOf("categories/") + 11,
+            window.location.href.length
+          ),
+          strictMode: false,
+        }
+      )
+      .then((res) => {
         setProducts(res.data.data.products);
-      })
+      });
 
-    axios.post(`${process.env.REACT_APP_ENDPOINT_URL}/api/v1/category/get/filters`, {
-      "category": window.location.href.slice(window.location.href.lastIndexOf('categories/') + 11, window.location.href.length).toLowerCase()
-    })
-      .then(res => {
+    axios
+      .post(
+        `${process.env.REACT_APP_ENDPOINT_URL}/api/v1/category/get/filters`,
+        {
+          category: window.location.href
+            .slice(
+              window.location.href.lastIndexOf("categories/") + 11,
+              window.location.href.length
+            )
+            .toLowerCase(),
+        }
+      )
+      .then((res) => {
         setFilters(res.data.data);
-      })
+      });
   }
 
   async function getProductsByFilter() {
-    if (valueFilter["priceMin"] == null) { valueFilter["priceMin"] = -1 }
-    if (valueFilter["priceMax"] == null) { valueFilter["priceMax"] = -1 }
-    axios.post(`${process.env.REACT_APP_ENDPOINT_URL}/api/v1/product/get/category/filter`, {
-      "category": window.location.href.slice(window.location.href.lastIndexOf('categories/') + 11, window.location.href.length).toLowerCase(),
-      "fullData": true,
-      "filter": valueFilter
-    })
-      .then(res => {
+    var a = valueFilter;
+    if (a["priceMin"] == null) {
+      a["priceMin"] = 0;
+    }
+    if (a["priceMax"] == null) {
+      a["priceMax"] = 100000;
+    }
+    axios
+      .post(
+        `${process.env.REACT_APP_ENDPOINT_URL}/api/v1/product/get/category/filter`,
+        {
+          category: window.location.href
+            .slice(
+              window.location.href.lastIndexOf("categories/") + 11,
+              window.location.href.length
+            )
+            .toLowerCase(),
+          fullData: true,
+          filter: a,
+        }
+      )
+      .then((res) => {
         setProducts(res.data.data.products);
       });
   }
@@ -66,24 +107,57 @@ function CategoryProducts() {
     if (filters) {
       for (var key of Object.keys(filters)) {
         var a = key;
-        x.push(<><div className="flex flex-col items-start space-y-2">
-          <label for={key}>{key}</label>
-          <select className="border border-gray-600 rounded-lg" name={a} id={a} onChange={(val) => changeFilter(a, val)} value={valueFilter[a]}>
-            <option value="" >Default</option>
-            {filters[a].map((f, index) => { return (<option value={f}>{f}</option>); })}
-          </select>
-        </div>
-        </>);
+        x.push(
+          <>
+            <div className="flex flex-col items-start space-y-2">
+              <label for={key}>{key}</label>
+              <select
+                className="border border-gray-600 rounded-lg"
+                name={a}
+                id={a}
+                onChange={(val) => changeFilter(a, val)}
+                value={valueFilter[a]}
+              >
+                <option value="">Default</option>
+                {filters[a].map((f, index) => {
+                  return <option value={f}>{f}</option>;
+                })}
+              </select>
+            </div>
+          </>
+        );
       }
-      x.push(<><div className="flex flex-col items-start space-y-2"><label for={"priceMin"}>Minimum Price</label>
-        <input type="text" name="priceMin" className="border border-gray-600 rounded-lg" onChange={(val) => changeFilter(a, val)} value={valueFilter["priceMin"]} />
-      </div></>);
-      x.push(<><div className="flex flex-col items-start space-y-2">
-        <label for={"priceMax"}>Maximum Price</label>
-        <input type="text" name="priceMax" className="border border-gray-600 rounded-lg" onChange={(val) => changeFilter(a, val)} value={valueFilter["priceMax"]} /></div></>);
+      x.push(
+        <>
+          <div className="flex flex-col items-start space-y-2">
+            <label for={"priceMin"}>Minimum Price</label>
+            <input
+              type="text"
+              name="priceMin"
+              className="border border-gray-600 rounded-lg"
+              onChange={(val) => changeFilter(a, val)}
+              value={valueFilter["priceMin"]}
+            />
+          </div>
+        </>
+      );
+      x.push(
+        <>
+          <div className="flex flex-col items-start space-y-2">
+            <label for={"priceMax"}>Maximum Price</label>
+            <input
+              type="text"
+              name="priceMax"
+              className="border border-gray-600 rounded-lg"
+              onChange={(val) => changeFilter(a, val)}
+              value={valueFilter["priceMax"]}
+            />
+          </div>
+        </>
+      );
     }
     return x;
-  }
+  };
   const Products = () => {
     if (products && products.length >= 1) {
       return products.map((product, index) => {
--- ecommerce-react/src/Products.js
+++ ecommerce-react/src/Products.js
@@ -3,6 +3,7 @@ import HomeProduct from "./HomeProduct";
 import { useLocation } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
 function Products() {
   const [itemStyle, setItemStyle] = useState(
@@ -11,6 +12,7 @@ function Products() {
 
   const [products, setProducts] = useState([]);
   const [filters, setFilters] = useState([]);
+  const [priceDown, setPriceDown] = useState(false);
 
   const location = useLocation();
   const categories = useSelector((state) => state.categories);
@@ -44,12 +46,27 @@ function Products() {
     } else return null;
   };
 
+  const sortPrice = () => {
+    setPriceDown(!priceDown);
+    if (priceDown) products.sort((a, b) => (a.price > b.price ? 1 : -1));
+    else products.sort((a, b) => (a.price < b.price ? 1 : -1));
+  };
+
   return (
     <>
       <div className="lg:mx-20 h-full flex flex-col">
         <div className="flex">
           <div className="flex-shrink flex-grow mr-4 w-4/5">
-            <div className="flex mt-10 justify-end mx-8"></div>
+            <div className="flex mt-10 justify-end mx-8 my-4">
+              <button onClick={() => sortPrice()} className="flex">
+                Sort By Price
+                {priceDown ? (
+                  <FaAngleDown size="25" />
+                ) : (
+                  <FaAngleUp size="25" />
+                )}
+              </button>
+            </div>
             <div className="flex  flex-wrap">
               <Products />
             </div>
